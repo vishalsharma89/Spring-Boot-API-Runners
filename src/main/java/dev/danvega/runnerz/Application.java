3d6686508ae7f6a24224533a0e7fc128f4bf4f2f
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2024-07-10 01:28:44 +0530

    Add files via upload
    REST API using spring boot

---
+++ src/main/java/dev/danvega/runnerz/Application.java
@@ -0,0 +1,25 @@
+package dev.danvega.runnerz;
+
+import dev.danvega.runnerz.user.UserHttpClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	UserHttpClient userHttpClient() {
+	    RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+	    HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+	    return factory.createClient(UserHttpClient.class);
+	}
+}
